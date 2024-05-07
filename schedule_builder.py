b0a931ee406273dd1d7d70c1a3a458e42e62134b
Reserved by Dev <dperry14@terpmail.umd.edu>
2024-05-07 12:19:29 -0400

    fixed schedule, save/load function and catalog

--- schedule_builder.py
+++ schedule_builder.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
-
+import json
+import os
 
 
 def classOrganizer(schedule):
@@ -30,10 +31,14 @@ class Schedule:
         inputed into the list.
         '''        
         self.name = name
-        self.schedule = []
+        if not os.path.exists(f"{self.name}_schedule.json"):
+            self.schedule = []
+            print(f'No existing schedule found for {self.name}. Starting with an empty schedule.')
+        else:
+            self.load_schedule()
     
     def coursecode_validation(self, course_code):
-        """Validates the course code format.
+        """Validates the course code format while also ensuring that the inputted course code exists in UMD's course catalog.
         
         Args: 
             course_code (str): course code validation.
@@ -41,13 +46,19 @@ class Schedule:
         Returns: 
             bool: True if the input course code is valid, otherwise False.
         """
-        expr = r"""(?x)
-^(?P<course_code>[a-zA-Z]{4}\d{3}$)"""
-        return bool(re.match(expr, course_code))
-    
+        courseid = []
+        with open('202008.json','r') as f1:
+            courses = json.load(f1)
+            for course in courses:
+                courseid.append(course['course_id'])
+        if course_code in courseid:
+                expr = r"""(?x)^(?P<course_code>[a-zA-Z]{4}\d{3}$)"""
+                return bool(re.match(expr, course_code))
+        else:
+            print(['Course code not found in course catalog. Enter a valid course code'])
     def add_class(self):
         '''Method used to add classes to the self.schedule attribute. 
-        '''        
+        '''      
         while True:
             course_code = input('Course code: ')
             if self.coursecode_validation(course_code):
@@ -55,14 +66,36 @@ class Schedule:
             else:
                 print("Invalid course code. Enter a valid course code.")
         credits = input('Credits: ')
-        day = input('Day: ')
+        days = input('Days: ')
         time = input('Time: ')
-        self.schedule.append({'Course code': course_code, 'Credits': credits, 'Day': day, 'Time': time})
+        self.schedule.append({'Course code': course_code, 'Credits': credits, 'Days': days, 'Time': time})
+        
+        self.save_schedule()
+    
+    def save_schedule(self):
+        '''Saves the student schedule
+        '''        
+        with open(f'{self.name}_schedule.json','w') as f2:
+            json.dump(self.schedule, f2)
+    
+    def load_schedule(self):
+        '''Loads the student schedule
+        '''        
+        try:
+            with open(f"{self.name}_schedule.json", 'r') as file:
+                self.schedule = json.load(file)
+        except FileNotFoundError:
+            self.schedule=[]
+            print('New schedule created')
             
+    
     def clear_schedule(self):
         '''Method to clear the self.schedule object. 
-        '''        
+        ''' 
+        self.load_schedule()      
         self.schedule = []
+        self.save_schedule()
+        
         
     def drop_class(self, code):
         '''Method used to drop a class from the schedule attribute
@@ -71,13 +104,15 @@ class Schedule:
             code (str): Course code to be removed from the attribute. Breaks out
             of the loop once it is found because each course should only be 
             listed once.
-        '''        
+        ''' 
+        self.load_schedule()      
         for course in self.schedule:
             if course['Course code'] == code:
                 self.schedule.remove(course)
                 break
         else:
             print("Course not found in schedule.")
+        self.save_schedule()
             
     def show_schedule(self):
         '''Takes the schedule object and creates a dataframe object neatly 
@@ -95,13 +130,15 @@ class Schedule:
         return len(self.schedule)
     
 
-    # will need a method with a with stateent to store courses for self.schedule
+    # will need a method with a with statement to store courses for self.schedule
     # , that way self.schedule can maintain updates as we add and remove classes
 
 
 
+
 s1 = Schedule('Devin')
-s1.add_class()
+s1.clear_schedule()
+#s1.add_class()
 schedule_df = s1.show_schedule()
 if not schedule_df.empty:
     print(schedule_df)
